85650823bfddc99d5403749074b1741d8ba101c6
TinoMaster <oscarmarcos2673@gmail.com>
2023-04-25 19:22:40 -0400

    created functions about encrypt passwords

---
+++ utils/password.hash.js
@@ -0,0 +1,8 @@
+const bcrypt = require('bcrypt');
+
+async function hashPassword(pass) {
+  const hash = await bcrypt.hash(pass, 10);
+  return hash;
+}
+
+module.exports = hashPassword;
---
+++ utils/password.verify.js
@@ -0,0 +1,9 @@
+const bcrypt = require("bcrypt");
+
+async function verifyPassword(pass, hash) {
+  const isMatch = await bcrypt.compare(pass, hash);
+
+  return isMatch;
+}
+
+module.exports = verifyPassword;
